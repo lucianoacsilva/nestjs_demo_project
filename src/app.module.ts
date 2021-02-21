e428e5cf7be2419d3e064f58b955ffd5c398c922
Luciano Augusto Campagnoli Da Silva <lucianos@mcity73.aquarius.cpqd.com.br>
2021-02-21 10:02:59 -0300

    Connects application to mongoDB database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot("mongodb+srv://lucigol:bigbrother@cluster0.iy7wv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
